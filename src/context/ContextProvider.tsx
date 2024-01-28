feaa94a5b81bc50741541ffc9167bd3841eb521b
rameshinumula <inumularamesh@gmail.com>
2024-01-28 08:52:57 +0530

    use context with state values.

--- src/context/ContextProvider.tsx
+++ src/context/ContextProvider.tsx
@@ -1,17 +1,42 @@
-import { createContext } from "react"
+import { createContext, useState } from "react"
 import { theme } from './themes';
 
 
 type providerPropsToUse = {
-    children: React.ReactNode;
+  children: React.ReactNode
 }
 
-export const getContextPrvd = createContext(theme);
+type userObj = {
+  name: string,
+  email: string
+}
+
+// INITIALIZE CONTEXT STATE TYPES
+type userContext = {
+  userVal: userObj | null,
+  //COPY THE TYPE OF SETUSER ON USESTATE WHILE HOVER ON setUser METHOD AND COPY
+  setUser: React.Dispatch<React.SetStateAction<userObj | null>>,
+  // THEMES STYLES TYPES DECLARATION
+  theme: {
+    primary: {
+      mainBackground: string,
+      mainText: string
+    }
+    secondry: {
+      backGrndColor: string,
+      text: string
+    }
+  }
+}
+
+export const getContextPrvd = createContext({} as userContext);
 
 export const ContextProvider = ({ children }: providerPropsToUse) => {
-return(
-    <getContextPrvd.Provider value={theme}>
-        {children}
+  const [userVal, setUser] = useState<userObj | null>(null)
+  return (
+    // USE CONTEXT VALUES IN VALUE PROP TO ACCESS WHEREEVER U WANT
+    <getContextPrvd.Provider value={{ userVal, setUser, theme }}>
+      {children}
     </getContextPrvd.Provider>
-)
+  )
 }
--- src/context/MainCom.tsx
+++ src/context/MainCom.tsx
@@ -2,10 +2,26 @@ import { useContext } from "react";
 import { getContextPrvd } from "./ContextProvider";
 
 export const MainCom = () => {
-    const mainStyContext = useContext(getContextPrvd);
-    return (
-        <div style={{ backgroundColor: mainStyContext?.primary?.mainBackground, color: mainStyContext?.primary?.mainText }}>
-            <h2>this is the context provider styles we are using</h2>
-        </div>
-    )
+  const mainStyContext = useContext(getContextPrvd);
+  const handleLogin = () =>{
+    mainStyContext.setUser({
+      name: 'Hello Ramesh',
+      email: 'inumularamesh1@gmail.com'
+    })
+  }
+  const handleLogout = () => {
+    mainStyContext.setUser({
+      name: '',
+      email: ''
+    })
+  }
+  return (
+    <div style={{ backgroundColor: mainStyContext.theme.primary.mainBackground, color: mainStyContext?.theme.primary?.mainText }}>
+    <h2>this is the context provider styles we are using</h2>
+      <button onClick={handleLogin}>login</button>
+      <button onClick={handleLogout}>logout</button>
+      <h3>User Name: {mainStyContext.userVal?.name}</h3>
+      <h3>User Email: {mainStyContext.userVal?.email}</h3>
+    </div>
+  )
 }
